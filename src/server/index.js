51c08385f5f591dfa85b0ec24bf4e233991a36bd
Johannes <74767916+Ranarim@users.noreply.github.com>
2022-03-14 21:11:08 +0100

    Commting my first Steps

--- src/server/index.js
+++ src/server/index.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch')
 const path = require('path')
 
 const app = express()
-const port = 3000
+const port = 3000;
 
 // setup the ability to see into response bodies
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -15,11 +15,11 @@ app.use(bodyParser.json())
 app.use('/', express.static(path.join(__dirname, '../client')))
 
 // API calls ------------------------------------------------------------------------------------
-app.get('/', async (req, res) => {
+app.get('/', async(req, res) => {
     res.sendFile(path.join(__dirname, '../client/pages/home.html'));
 })
 
-app.get('/race', async (req, res) => {
+app.get('/race', async(req, res) => {
     res.sendFile(path.join(__dirname, '../client/pages/race.html'));
 })
 
